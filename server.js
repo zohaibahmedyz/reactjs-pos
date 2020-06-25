c166cc035d06d14cbc67332454a82705fc72a6ef
Zohaib <zohaibahmed155@gmail.com>
2020-06-25 22:15:30 +0500

    test commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGODB_URI ||
     {useNewUrlParser : true, useUnifiedTopology: true }
 ).
 then(()=>{
-    console.log("Connected to DB!")
+    console.log("Connected to MongoDB!")
 }).
 catch(err =>{
     console.log(err)
