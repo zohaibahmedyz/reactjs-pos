eed166e5b5bcdc53f5d7a76cd8c68584d32957a6
Zohaib <zohaibahmed155@gmail.com>
2020-06-25 22:48:10 +0500

    Test client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component {
+  state = {
+    users: [],
+  };
+  componentDidMount() {
+    axios.get("https://usmanpos.herokuapp.com/products/").then((response) => {
+      this.setState({ users: response.data });
+    });
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li className="user">
+              <p>
+                <strong>Name:</strong> {user.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {user.sellingPrice}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
